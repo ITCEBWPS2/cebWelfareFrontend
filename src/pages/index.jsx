4930f8ae014d6ca79b42b1e78d9dcae5aafbd06a
Elvitigala E.D.T.V <99307615+IT21176838@users.noreply.github.com>
2024-09-07 14:35:57 +0530

    changed index extention to jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,38 @@
+import Home from "./Home.jsx";
+import Benefits from "./Benefits.jsx";
+import Contact from "./Contact.jsx";
+import About from "./About.jsx";
+import Loans from "./Loans.jsx";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
+
+import MemberBenefits from "./members/MemberBenefits.jsx";
+import MemberLoans from "./members/MemberLoans.jsx";
+import Members from "./members/Members.jsx";
+import RegisterMember from "./members/RegisterMember.jsx";
+import Relatives from "./members/Relatives.jsx";
+import ViewMember from "./members/ViewMember.jsx";
+import LoanApplication from "./members/LoanApplication.jsx";
+import Carousel from "../components/Carousel.jsx";
+import HorizontalCarousel from "../components/HorizontalCarousel.jsx";
+export { default as AdminPanel } from "./AdminPanel.jsx";
+export { default as ViewUsers } from "./ViewUsers.jsx";
+
+export {
+  Home,
+  Benefits,
+  Contact,
+  About,
+  Loans,
+  Login,
+  Register,
+  MemberBenefits,
+  MemberLoans,
+  Carousel,
+  HorizontalCarousel,
+  LoanApplication,
+  Members,
+  RegisterMember,
+  Relatives,
+  ViewMember,
+};
