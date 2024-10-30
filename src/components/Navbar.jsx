935b43952a08518fe0fead12b238ffdedc934b6e
Hasitha <hasithapeiris.me@gmail.com>
2024-10-30 22:38:20 +0530

    fix: make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 // src/components/Navbar.jsx
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -14,18 +22,24 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-red-900 p-4">
+    <nav className="bg-red-900 py-4">
       <div className="wrapper-nav flex-between">
         {/* Logo Section */}
-        <div className="flex-center gap-3">
-          <img src="/Picture2.jpg" alt="CEB Logo" className="h-12 w-12" />
-          <div className="text-yellow-300 text-lg font-semibold">
-            <Link to="/">Welfare Society CEB (WPS II)</Link>
+        <Link to="/">
+          <div className="flex-center gap-2">
+            <img
+              src="/Picture2.jpg"
+              alt="CEB Logo"
+              className="h-8 w-8 md:w-12 md:h-12"
+            />
+            <div className="text-yellow-300 text-lg font-semibold">
+              CEB (WPS II)
+            </div>
           </div>
-        </div>
+        </Link>
 
         {/* Navigation Links */}
-        <div className="space-x-6 font-medium">
+        <div className="hidden md:block space-x-6 font-medium">
           <Link to="/" className="text-white hover:text-yellow-300">
             Home
           </Link>
@@ -48,6 +62,73 @@ const Navbar = () => {
           <button onClick={handleLogout} className="button-yellow-outline">
             Login
           </button>
+        </div>
+
+        {/* Mobile Navigation */}
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <Menu className="w-6 h-6 text-white" />
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="text-left bg-red-900 border-none"
+            >
+              <Link to="/">
+                <div className="flex items-center mb-7 mt-2 gap-2">
+                  <img
+                    src="/Picture2.jpg"
+                    alt="CEB Logo"
+                    className="h-12 w-12"
+                  />
+                  <div className="text-yellow-300 text-lg font-semibold">
+                    CEB (WPS II)
+                  </div>
+                </div>
+              </Link>
+              <SheetHeader className="text-left">
+                <SheetTitle className="text-white dark:text-white">
+                  Menu
+                </SheetTitle>
+              </SheetHeader>
+              <nav className="flex flex-col space-y-4 mt-5">
+                <Link to="/" className="text-white hover:text-yellow-300">
+                  Home
+                </Link>
+                <Link
+                  to="/members"
+                  className="text-white hover:text-yellow-300"
+                >
+                  Members
+                </Link>
+                <Link
+                  to="/Benefits"
+                  className="text-white hover:text-yellow-300"
+                >
+                  Benefits
+                </Link>
+                <Link to="/loans" className="text-white hover:text-yellow-300">
+                  Loans
+                </Link>
+                <Link to="/about" className="text-white hover:text-yellow-300">
+                  About
+                </Link>
+                <Link
+                  to="/contact"
+                  className="text-white hover:text-yellow-300"
+                >
+                  Contact
+                </Link>
+                {/* Logout button */}
+                <button
+                  onClick={handleLogout}
+                  className="button-yellow-outline"
+                >
+                  Login
+                </button>
+              </nav>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </nav>
