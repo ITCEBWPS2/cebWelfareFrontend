a4356ae385c89287eb74d442d8671a58ccf45117
Hasitha <hasithapeiris.me@gmail.com>
2024-11-02 23:51:21 +0530

    feat: complete links section

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -38,7 +38,7 @@ const LinksSection = () => {
   ];
 
   return (
-    <div className="relative bg-gray-100 bg-[url('/main_header_1.webp')] bg-cover bg-bottom mt-14 py-24 flex items-center justify-center">
+    <div className="relative bg-gray-100 bg-[url('/main_header_1.webp')] bg-cover bg-center mt-14 py-24 flex items-center justify-center">
       <div className="absolute inset-0 bg-gradient-to-b from-white to-transparent"></div>
       <div className="wrapper relative z-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-14">
