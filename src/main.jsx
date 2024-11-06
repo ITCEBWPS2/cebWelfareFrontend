cdb149fc96129f7312bd2d6c74e8cecf1df5a1ca
Hasitha <hasithapeiris.me@gmail.com>
2024-11-06 23:48:30 +0530

    feat: initialize react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Toaster position="top-center" reverseOrder={false} />
-    <App />
+    <Provider store={store}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
