f833cefaa79b2e5b0afe2eb2389d4c15eb4979f4
Hasitha <hasithapeiris.me@gmail.com>
2024-11-08 16:03:31 +0530

    fix: fix base URL

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,7 @@
+import { BASE_URL } from "@/constants";
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
   baseQuery,
