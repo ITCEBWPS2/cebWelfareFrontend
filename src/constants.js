c4a68a6a20bb73520bf2698b12fc4a6390b86a92
Elvitigala E.D.T.V <99307615+IT21176838@users.noreply.github.com>
2024-11-11 15:01:23 +0530

    changed the backend uri

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 // export const BASE_URL = "http://localhost:5000";
 
-export const BASE_URL = "https://ceb-welfare-backend.onrender.com";
+// export const BASE_URL = "https://ceb-welfare-backend.onrender.com";
+
+export const BASE_URL = "https://serverbackend-4wcf.onrender.com";
