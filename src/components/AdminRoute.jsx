77ca5e724683258c98f0117957cbaeb53fd7149d
Hasitha <hasithapeiris.me@gmail.com>
2024-12-29 15:39:04 +0530

    fix: update admin route

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -3,7 +3,19 @@ import { Outlet } from "react-router-dom";
 
 const AdminRoute = () => {
   const { user } = useAuth();
-  return user && user.role === "admin" ? (
+
+  const allowedRoles = [
+    "admin",
+    "super_admin",
+    "president",
+    "vice_president",
+    "secretary",
+    "assistant_secretary",
+    "treasurer",
+    "assistant_treasurer",
+  ];
+
+  return allowedRoles.includes(user?.role) ? (
     <Outlet />
   ) : (
     <div>Only admins have access to this page.</div>
