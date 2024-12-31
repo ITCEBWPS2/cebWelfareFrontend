ccf48cb13c60c418130962998f45204bd75f4042
Hasitha <hasithapeiris.me@gmail.com>
2024-12-31 12:56:49 +0530

    fix: fix benefits table user profile navigation

--- src/components/LoansTable.jsx
+++ src/components/LoansTable.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "@/api/authContext";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "@/constants";
 import {
